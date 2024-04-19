2a2afcdd6e9dfa5e73274132c5334ed3b78124b3
Quan-XH <49560469+Quan-XH@users.noreply.github.com>
2024-04-20 00:18:53 +0800

    Add files via upload

---
+++ src/views/vuex.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        vuex
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import axios from '../utils/axios';
+
+export default defineComponent({
+    setup() {
+        axios.get('/user/XPoet').then(res => {
+            console.log(res,'res')
+        }).catch(err => {
+            console.log(err,'err')
+        })
+    }
+})
+</script>
+
+<style lang="stylus">
+
+</style>
